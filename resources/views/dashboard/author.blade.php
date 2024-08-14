33dfe59d21e8a2f89243b36449f6024caa234120
arieeefajar <ariefajar739@gmail.com>
2024-08-14 17:40:33 +0700

    refactory: [view] dashboard author

--- resources/views/dashboard/author.blade.php
+++ resources/views/dashboard/author.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard Author</h1>
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <p class="mb-0 text-gray-800">Selamat Datang {{ Auth::user()->fullname }}</p>
     </div>
 
     <!-- Content Row -->
